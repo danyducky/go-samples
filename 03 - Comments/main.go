b8fdc8bbccc7b7a6bbd15a6a666b31882f0b854b
Danil Morikov <danyducky@Danils-MBP.routers.saritasa.io>
2024-02-10 21:15:40 +0300

    Comments samples

---
+++ 03 - Comments/main.go
@@ -0,0 +1,8 @@
+package main // Current file package.
+
+import "fmt" // Include 'fmt' package into current file package.
+
+// Program entry point.
+func main() {
+	fmt.Println("Hello Go!") // Inline output.
+}
